e49bbca2d1252685156490958e097398e42385d4
sheikh11714 <137689936+sheikh11714@users.noreply.github.com>
2023-06-28 09:38:38 +0330

    Update Login_TestCase.py

--- Tests/Login_TestCase.py
+++ Tests/Login_TestCase.py
@@ -1,14 +1,15 @@
+from Login import Login
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from Pages.Login import Login
-import time
+
+from time import sleep
+
 
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-
-driver.get("http://172.20.12.43/")
-
+driver.get("http://172.20.12.43")
+driver.implicitly_wait(2)
 login = Login(driver=driver)
 login.enter_username("sabadmin")
 login.enter_password("sabadmin")
 login.click_on_Login_button()
-time.sleep(2)
+sleep(2)
